c5922a1d8d2d100bd4117edf76f520a0ee897698
SpyerNot <lowkimsenglowkimseng8@gmail.com>
2025-06-10 16:20:07 +0800

    update

--- pages/audio to text.py
+++ pages/audio to text.py
@@ -1,3 +1,4 @@
 import streamlit as st
 st.title("Welcome to the page Audio to Text!!")
 st.write("This page allows whatever you say and turn it into a text!")
+st.audio_input()
--- pages/text to audio.py
+++ pages/text to audio.py
@@ -1,3 +1,4 @@
 import streamlit as st
 st.title("Welcome to Text to Audio translator!!")
 st.write("This page allows whatever you type and turn it into an audio")
+st.chat_input()
