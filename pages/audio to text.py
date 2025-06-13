5fbe3c4cea1ea555bdf60bd718042c4b755f6ba9
SpyerNot <lowkimsenglowkimseng8@gmail.com>
2025-06-13 23:06:10 +0800

    updated line 65 and line 1

--- pages/audio to text.py
+++ pages/audio to text.py
@@ -1,4 +1,4 @@
-from st_audiorecorder import audio_recorder
+from audiorecorder import audiorecorder
 
 import streamlit as st
 import io
@@ -62,7 +62,7 @@ st.markdown("<h3 style='text-align: center; color: grey;'>OR</h3>", unsafe_allow
 
 st.subheader("Option 2: Record Audio Directly")
 
-recorded_audio_bytes = audio_recorder(
+recorded_audio_bytes = audiorecorder(
     text="Click to Record",
     recording_color="#e8b62c",
     neutral_color="#6a6a6a",
